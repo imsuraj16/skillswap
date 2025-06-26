849f89873559df954590791f2e7b170c29dbfd14
Surajverma <sv63619@gmail.com>
2025-06-26 13:42:40 +0530

    done

--- frontend/src/api/apiconfig.jsx
+++ frontend/src/api/apiconfig.jsx
@@ -2,7 +2,7 @@ import axios from "axios"
 
 const instance = axios.create({
 
-    baseURL : "http://localhost:3000/"
+    baseURL : "https://skillswap-backend-9uvh.onrender.com"
 })
 
 export default instance
